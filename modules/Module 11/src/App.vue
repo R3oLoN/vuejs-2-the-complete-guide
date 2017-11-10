a8f1a8a8336bbe78d44c75807270b49b778739cc
R3oLoN <leandroreolon@gmail.com>
2017-11-09 22:46:36 -0200

    Module 11: Finished module

--- modules/Module 11/src/App.vue
+++ modules/Module 11/src/App.vue
@@ -10,21 +10,24 @@
                         <input
                                 type="text"
                                 id="email"
-                                class="form-control">
+                                class="form-control"
+								v-model.lazy="userData.email">
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
                         <input
                                 type="password"
                                 id="password"
-                                class="form-control">
+                                class="form-control"
+								v-model.lazy="userData.password">
                     </div>
                     <div class="form-group">
                         <label for="age">Age</label>
                         <input
                                 type="number"
                                 id="age"
-                                class="form-control">
+                                class="form-control"
+								v-model="userData.age">
                     </div>
 
                 </div>
@@ -36,7 +39,8 @@
                     <textarea
                             id="message"
                             rows="5"
-                            class="form-control"></textarea>
+                            class="form-control"
+							v-model="message"></textarea>
                 </div>
             </div>
             <div class="row">
@@ -46,13 +50,15 @@
                             <input
                                     type="checkbox"
                                     id="sendmail"
-                                    value="SendMail"> Send Mail
+                                    value="SendMail"
+									v-model="sendMail"> Send Mail
                         </label>
                         <label for="sendInfomail">
                             <input
                                     type="checkbox"
                                     id="sendInfomail"
-                                    value="SendInfoMail"> Send Infomail
+                                    value="SendInfoMail"
+									v-model="sendMail"> Send Infomail
                         </label>
                     </div>
 
@@ -64,13 +70,15 @@
                         <input
                                 type="radio"
                                 id="male"
-                                value="Male"> Male
+                                value="Male"
+								v-model="gender"> Male
                     </label>
                     <label for="female">
                         <input
                                 type="radio"
                                 id="female"
-                                value="Female"> Female
+                                value="Female"
+								v-model="gender"> Female
                     </label>
                 </div>
             </div>
@@ -79,39 +87,46 @@
                     <label for="priority">Priority</label>
                     <select
                             id="priority"
-                            class="form-control">
-                        <option></option>
+                            class="form-control"
+							v-model="selectedPriority">
+                        <option v-for="priority in priorities">{{ priority }}</option>
                     </select>
                 </div>
             </div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
+					<app-switch v-model="dataSwitch"></app-switch>
+				</div>
+			</div>
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
-                            class="btn btn-primary">Submit!
+                            class="btn btn-primary"
+							@click.prevent="submitted">Submit!
                     </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Mail:</p>
-                        <p>Password:</p>
-                        <p>Age:</p>
-                        <p>Message: </p>
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: {{ userData.password }}</p>
+                        <p>Age: {{ userData.age }}</p>
+                        <p style="white-space: pre;">Message: {{ message }}</p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
-                            <li></li>
+                            <li v-for="item in sendMail">{{ item }}</li>
                         </ul>
-                        <p>Gender:</p>
-                        <p>Priority:</p>
-                        <p>Switched:</p>
+                        <p>Gender: {{ gender }}</p>
+                        <p>Priority: {{ selectedPriority }}</p>
+                        <p>Switched: {{ dataSwitch }}</p>
                     </div>
                 </div>
             </div>
@@ -120,8 +135,33 @@
 </template>
 
 <script>
-    export default {
-    }
+import AppSwitch from './components/Switch.vue'
+export default {
+	components: {
+		AppSwitch
+	},
+	data() {
+		return {
+			userData: {
+				email: '',
+				password: '',
+				age: 27
+			},
+			message: 'A new Text',
+			sendMail: [],
+			gender: 'Male',
+			selectedPriority: 'High',
+			priorities: ['High', 'Medium', 'Low'],
+			dataSwitch: true,
+			isSubmitted: false
+		}
+	},
+	methods: {
+		submitted() {
+			this.isSubmitted = true
+		}
+	}
+}
 </script>
 
 <style>
